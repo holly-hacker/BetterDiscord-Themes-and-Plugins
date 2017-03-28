0bf20a399d868b98ff5af590220b1ca4522d8c95
Mydayyy <mydayyy@outlook.com>
2017-03-28 19:50:48 +0200

    Plugin working again for the most recent discord update. It's not the cleanest solution as we have hardcoded the path to the function, but it gets the job done for now. This will fix HoLLy-HaCKeR/BetterDiscord-Themes-and-Plugins#2

--- Plugins/lazyload_patcher.plugin.js
+++ Plugins/lazyload_patcher.plugin.js
@@ -2,18 +2,20 @@
 
 //jshint esversion: 6
 
-//TODO: somehow reload/redraw the Channels object, for seamless patching
-//TODO: find Channels prototype without it being added to the DOM, also for seamless patching
+// This is not the cleanest code, as we now have hardcoded the path to the getRowHeight function, but it will get the job done for now
+
 
 var lazyload_patcher = function() {
 	this.pluginName = 'LazyLoad Patcher';
 
 	this.getName = 			function()	{return this.pluginName;};
-	this.getDescription = 	function()	{return 'LazyLoad Patcher - Patches Discord\'s lazy loading to allow for themes that modify channel heights.</br></br>Credits to noodlebox#0155 for their help and code on react objects.';};
-	this.getVersion = 		function()	{return '1.0';};
+	this.getDescription = 	function()	{return 'LazyLoad Patcher - Patches Discord\'s lazy loading to allow for themes that modify channel heights.</br></br>Credits to noodlebox#0155 for their help and code on react objects. 3/25 fix by Mydayyy#0344';};
+	this.getVersion = 		function()	{return '1.1';};
 	this.getAuthor = 		function()	{return '<a href="http://JustM3.net">HoLLy#2750</a>';};
 
-	this.patches = [{selector: ".guild-channels", funcName: "getRowHeight", storageName: "channelRowHeight", default: 36}];
+	this.patches = [{selector: ".guild-channels", funcName: "getRowHeight", storageName: "channelRowHeight", default: 36, patchFunction: function(selector, funcName, storageName) {this.patchRowHeight(selector, funcName, storageName);}.bind(this)}];
+
+	var ctr = 0;
 
 	this.load = function()	{
 		this.Log("Loaded");
@@ -27,6 +29,7 @@ var lazyload_patcher = function() {
 
 	this.start = function() {
 		this.Log("Started");
+
 
 		//if we start up to the friends page, channels won't be loaded
 		if (location.pathname.startsWith('/channels/@me')) {
@@ -47,60 +50,52 @@ var lazyload_patcher = function() {
 	this.unload = function(){	this.Log("Unloaded");	};
 
 	this.onMessage = function() {};
-	this.onSwitch = function()  {};
+	this.onSwitch = function()  {
+		// Discord deletes the guild-channel node when switching to DM's. We need to watch onSwitch and reapply our fix.
+		if (location.pathname.startsWith('/channels/@me')) {
+			ctr = 1;
+		} else {
+			if (ctr > 0 ) {
+				this.doChatPatch();
+				ctr = 0;
+			}
+		}
+	};
+
 	this.observer = function(e) {};
 
 	this.getSettingsPanel = function () {
-		var str = `<b>Enter new channel height: </b><input type="number" id="llInputChannelHeight" 
+		var str = `<b>Enter new channel height: </b><input type="number" id="llInputChannelHeight"
 		value="${bdPluginStorage.get(this.pluginName, 'channelRowHeight')}" /><br>
 		<!-- more options go here, some day <br> -->
 		<br><button onClick="llSave()">Save new settings</button><br>`;
 		var js = `<script>
-				function llSave() { 
-					var ch = parseInt($('#llInputChannelHeight').val()); 
-					if (ch != null && ch != "") { 
-						bdPluginStorage.set("${this.pluginName}", "channelRowHeight", ch); 
-						alert("Please restart the plugin to apply changes.") 
-					} 
+				function llSave() {
+					var ch = parseInt($('#llInputChannelHeight').val());
+					if (ch != null && ch != "") {
+						bdPluginStorage.set("${this.pluginName}", "channelRowHeight", ch);
+						alert("Please restart the plugin to apply changes.")
+					}
 				}</script>`;
 		var info = 'Channel height is 36 by default, 28 for minimal mode.';
 
 		return str+js+info;
 
-	};	//TODO: make proper
+	};
 
 	this.doChatPatch = function() {
-
-		//this.Log('in doChatPatch right now, this is ' + this.constructor.name);
 		for (var i = 0; i < this.patches.length; i++) {
 			var patch = this.patches[i];
-			this.patchSomething(patch.selector, patch.funcName, patch.storageName);
+			this.patchSomething(patch.selector, patch.funcName, patch.storageName, patch.patchFunction);
 		}
 		this.Log('finished doChatPatch');
 	};
 
-	this.patchSomething = function(selector, funcName, storageName) {
+	this.patchSomething = function(selector, funcName, storageName, patchFunction) {
 		try {
-			//get stuff, make stuff
-			var instList = $(selector);
-			if (instList.length === 0) throw "Could not find selector.";
-			var inst = getOwnerInstance(instList[0]);
-
-			var newVar = (bdPluginStorage.get(this.pluginName, storageName));
-			var patchedFunc = function() {return newVar;};
-
-			//patch current instance
-			inst[funcName] = patchedFunc;
-
-			//patch new instances, credits to noodlebox
-			var index = inst.__reactAutoBindPairs.findIndex(n => n === funcName);
-			if (index == -1) throw "couldn't find " + funcName + " in __reactAutoBindPairs for selector" + selector;
-			inst.constructor.prototype[funcName] = inst.constructor.prototype.__reactAutoBindPairs[index+1] = patchedFunc;
-
-			//success
+			patchFunction(selector, funcName, storageName);
 			this.Log("Patched " + funcName);
 		} catch(err) {
-			//something went wrong. I should make this more verbose
 			this.Log("Failed to patch " + funcName + ": " + err.message, "error");
 		}
 	};
@@ -109,57 +104,17 @@ var lazyload_patcher = function() {
 		console[method]("%c[" + this.pluginName + "]%c " + msg, "color: #DABEEF; font-weight: bold;", "");
 	};
 
-	//code by noodlebox
-	this.getOwnerInstance = function(e, {include, exclude=["Popout", "Tooltip", "Scroller", "BackgroundFlash"]} = {}) {
-	    if (e === undefined) {
-	        return undefined;
-	    }
+	this.patchRowHeight = function(selector, funcName, storageName) {
+		var instList = $(selector);
+		if (instList.length === 0) throw "Could not find selector.";
 
-	    // Set up filter; if no include filter is given, match all except those in exclude
-	    const excluding = include === undefined;
-	    const filter = excluding ? exclude : include;
+		var newVar = (bdPluginStorage.get(this.pluginName, storageName));
+		var patchedFunc = function() {return newVar;};
 
-	    // Get displayName of the React class associated with this element
-	    // Based on getName(), but only check for an explicit displayName
-	    function getDisplayName(owner) {
-	        const type = owner._currentElement.type;
-	        const constructor = owner._instance && owner._instance.constructor;
-	        return type.displayName || constructor && constructor.displayName || null;
-	    }
-	    // Check class name against filters
-	    function classFilter(owner) {
-	        const name = getDisplayName(owner);
-	        return (name !== null && !!(filter.includes(name) ^ excluding));
-	    }
+		const getInternalInstance = e => e[Object.keys(e).find(k => k.startsWith("__reactInternalInstance"))];
+		var inst = getInternalInstance(instList[0]);
+		inst._currentElement._owner._currentElement._owner._currentElement._owner._instance.getRowHeight =  patchedFunc; // We hardcoded the path here.. probably need to adjust that more often now
+		inst._currentElement._owner._currentElement._owner._currentElement._owner._instance.handleListScroll(); // This will force a refresh. Otherwise we would have to scroll once in order for the missing channels to show up
 
-	    const getInternalInstance = e => e[Object.keys(e).find(k => k.startsWith("__reactInternalInstance"))];
-
-	    // Walk up the hierarchy until a proper React object is found
-	    for (let prev, curr=getInternalInstance(e); !_.isNil(curr); prev=curr, curr=curr._hostParent) {
-	        // Before checking its parent, try to find a React object for prev among renderedChildren
-	        // This finds React objects which don't have a direct counterpart in the DOM hierarchy
-	        // e.g. Message, ChannelMember, ...
-	        if (prev !== undefined && !_.isNil(curr._renderedChildren)) {
-	            /* jshint loopfunc: true */
-	            let owner = Object.values(curr._renderedChildren)
-	                .find(v => !_.isNil(v._instance) && v.getHostNode() === prev.getHostNode());
-	            if (!_.isNil(owner) && classFilter(owner)) {
-	                return owner._instance;
-	            }
-	        }
-
-	        if (_.isNil(curr._currentElement)) {
-	            continue;
-	        }
-
-	        // Get a React object if one corresponds to this DOM element
-	        // e.g. .user-popout -> UserPopout, ...
-	        let owner = curr._currentElement._owner;
-	        if (!_.isNil(owner) && classFilter(owner)) {
-	            return owner._instance;
-	        }
-	    }
-
-	    return null;
 	};
 };
